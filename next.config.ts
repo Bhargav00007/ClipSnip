a00e0b1d263abc55498e2da82e8a1d9a82a3f0c4
Bhargav Pattanayak <122164885+Bhargav00007@users.noreply.github.com>
2025-02-23 13:47:25 +0530

    Changes in config

--- next.config.ts
+++ next.config.ts
@@ -6,24 +6,11 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: "/downloads/:path",
+        source: "/downloads/:path*",
         headers: [
           { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,HEAD,OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "Range" },
-          { key: "Accept-Ranges", value: "bytes" },
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-      {
-        source: "/test/:path",
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,HEAD,OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "Range" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type,Range" },
           { key: "Accept-Ranges", value: "bytes" },
           {
             key: "Cache-Control",
@@ -33,17 +20,11 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-  images: {
-    unoptimized: true,
-  },
-  productionBrowserSourceMaps: process.env.NODE_ENV === "development",
+  images: { unoptimized: true },
   experimental: {},
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  outputFileTracingRoot: path.join(__dirname, "../../"),
 };
 
 export default nextConfig;
